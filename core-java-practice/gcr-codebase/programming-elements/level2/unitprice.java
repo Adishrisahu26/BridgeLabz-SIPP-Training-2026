2202fd86b53bec396e388cbcd5a0c823241534b7
Adishrisahu26 <adishrisahu26@gmail.com>
2026-06-09 21:52:26 +0530

    Add level2 programming-elements files

---
+++ core-java-practice/gcr-codebase/programming-elements/level2/unitprice.java
@@ -0,0 +1,10 @@
+package BridgeLabz-SIPP-Training-2026.core-java-practice.gcr-codebase.programming-elements.level2;
+
+public class unitprice {
+    public static void main(String[] args) {
+        double totalPrice = 100.0;
+        int quantity = 5;
+        double unitPrice = totalPrice / quantity;
+        System.out.println("Unit price: " + unitPrice);
+    }
+}
