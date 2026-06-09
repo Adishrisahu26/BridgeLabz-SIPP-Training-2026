42da05c49ab8019d91075147f797c862f343c5eb
Prachi Agarwal <prachi04agarwal@gmail.com>
2026-06-09 20:26:59 +0530

    Preserve control-flow and programming-elements structure, add level1/level2

---
+++ core-java-practice/gcr-codebase/programming-elements/level1/km_to_miles.java
@@ -0,0 +1,7 @@
+public class km_to_miles {
+    public static void main(String[] args) {
+        double kilometers = 10.0;
+        double miles = kilometers * 0.621371;
+        System.out.println(kilometers + " km = " + miles + " miles");
+    }
+}
