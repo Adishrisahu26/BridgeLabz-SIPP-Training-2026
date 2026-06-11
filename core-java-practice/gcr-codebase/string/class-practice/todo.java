b154b689a53cf125bd898acc90c0c9b4af347101
Adishrisahu26 <adishrisahuu26@gmail.com>
2026-06-11 12:38:11 +0530

    Add string/class-practice exercises

---
+++ core-java-practice/gcr-codebase/string/class-practice/todo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class todo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = "Hello World";   
+        s.toUpperCase();
+        
+        System.out.println(s);
+        s.toLowerCase();
+        System.out.println(s);
+    }
+}
